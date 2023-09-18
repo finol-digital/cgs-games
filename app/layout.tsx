955cbc2cebaa27052d9e607e22c1bda20b764bbf
David Finol <davidmfinol@gmail.com>
2023-09-17 19:49:18 -0500

    Add SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,21 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: {
+    default: 'CGS Games',
+    template: `%s | CGS Games`
+  },
+  description: 'Share your CGS games',
+  openGraph: {
+    title: 'CGS Games',
+    description: 'Share your CGS games',
+  },
+  robots: {
+    follow: true,
+    index: true
+  },
+}
+
 export default function RootLayout({
     children,
   }: {
