1db92c1b12d38cd165c54597afb4789fdad14a5a
David Finol <davidmfinol@gmail.com>
2023-09-18 08:33:01 -0500

    Add prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import Image from 'next/image'
+
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>
+  return (
+    <header><Image
+      src="/profile.png"
+      width={500}
+      height={500}
+      alt="Picture of the author"
+    /></header>
+  )
   }
