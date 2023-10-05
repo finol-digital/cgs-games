117098df781b6d279a1308eb1ac3641a40b0c5cb
David Finol <davidmfinol@gmail.com>
2023-10-05 10:53:40 -0500

    Develop components and pages

--- app/link/page.tsx
+++ app/link/page.tsx
@@ -1,3 +1,22 @@
+import Footer from "components/footer";
+import Image from "next/image";
+import cgsPic from "public/cgs.png";
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <div className="main-container">
+      <center>
+        <Image
+          src={cgsPic}
+          height="128"
+          alt="Share your Card Game Simulator (CGS) games"
+          priority={true}
+        />
+      </center>
+      <hr></hr>
+      <h1>Card Game Simulator (CGS) Games Link</h1>
+      <p>Add download links here</p>
+      <Footer />
+    </div>
+  );
 }
