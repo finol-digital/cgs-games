712da39d1f85c99c0e4082b5ba992b71f4873001
David Finol <davidmfinol@gmail.com>
2023-10-10 17:55:39 -0500

    Use typescript

--- components/gamesList.tsx
+++ components/gamesList.tsx
@@ -1,11 +1,16 @@
 import Link from "next/link";
 
-export default function GamesList({ games }: { games: any }) {
+interface Game {
+  slug: string;
+  name: string;
+}
+
+export default function GamesList({ games }: { games: Game[] }) {
   return (
     <ul>
       {games
-        .sort((a: any, b: any) => a.name.localeCompare(b.name))
-        .map((game: any) => (
+        .sort((a: Game, b: Game) => a.name.localeCompare(b.name))
+        .map((game: Game) => (
           <li key={game.slug}>
             <Link href={"/link/" + game.slug}>{game.name}</Link>
           </li>
