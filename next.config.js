e200350e76c58c2a71c733dd55fe4bb30e76614d
David Finol <davidmfinol@gmail.com>
2023-10-12 21:19:58 -0500

    Add comment

--- next.config.js
+++ next.config.js
@@ -12,6 +12,9 @@ module.exports = withPWA({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  //Static Website with these 2 lines and firebase.json source vs public
+  //output: "export",
+  //images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
