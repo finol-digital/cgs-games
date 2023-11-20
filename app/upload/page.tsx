2177bcdc92f6e86917d03f8efd18754958b715c4
David Finol <davidmfinol@gmail.com>
2023-11-19 19:22:07 -0600

    Develop

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,9 +1,7 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
+import { signInWithGoogle, signOut } from "@/lib/firebase/auth";
 import { Metadata } from "next";
-import Link from "next/link";
-
-// export const dynamic = "force-dynamic";
 
 export const metadata: Metadata = {
   title: "CGS Games Upload",
@@ -15,15 +13,44 @@ export const metadata: Metadata = {
 };
 
 export default async function Page() {
+  const user = null;
+  const username = null;
+  // 1. user signed out <SignInButton />
+  // 2. user signed in, but missing username <UsernameForm />
+  // 3. user signed in, has username <SignOutButton />
   return (
     <main className="main-container">
       <Header title="CGS Games Upload" />
-      <p>
-        The user upload functionality is still under construction! In the
-        meantime, if you would like to add your game to the list, please email{" "}
-        <Link href="mailto:david@finoldigital.com">david@finoldigital.com</Link>
-      </p>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
+
       <Footer />
     </main>
   );
+}
+
+// Sign in with Google button
+function SignInButton() {
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={"/google.png"} alt="Sign in with Google" /> Sign in with Google
+    </button>
+  );
+}
+
+// Allow setting their own username
+function UsernameForm() {
+  return null;
+}
+
+// Sign out button
+function SignOutButton() {
+  return <button onClick={() => signOut()}>Sign Out</button>;
 }
