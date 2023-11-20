82d40110eecba4731c93a734209f2cc3588c40ee
David Finol <davidmfinol@gmail.com>
2023-11-19 19:44:21 -0600

    Develop

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,6 +1,6 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
-import { signInWithGoogle, signOut } from "@/lib/firebase/auth";
+import UploadGameForm from "@/components/uploadGameForm";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -12,45 +12,12 @@ export const metadata: Metadata = {
   },
 };
 
-export default async function Page() {
-  const user = null;
-  const username = null;
-  // 1. user signed out <SignInButton />
-  // 2. user signed in, but missing username <UsernameForm />
-  // 3. user signed in, has username <SignOutButton />
+export default function Page() {
   return (
     <main className="main-container">
       <Header title="CGS Games Upload" />
-      {user ? (
-        !username ? (
-          <UsernameForm />
-        ) : (
-          <SignOutButton />
-        )
-      ) : (
-        <SignInButton />
-      )}
-
+      <UploadGameForm />
       <Footer />
     </main>
   );
-}
-
-// Sign in with Google button
-function SignInButton() {
-  return (
-    <button className="btn-google" onClick={signInWithGoogle}>
-      <img src={"/google.png"} alt="Sign in with Google" /> Sign in with Google
-    </button>
-  );
-}
-
-// Allow setting their own username
-function UsernameForm() {
-  return null;
-}
-
-// Sign out button
-function SignOutButton() {
-  return <button onClick={() => signOut()}>Sign Out</button>;
 }
---
+++ components/uploadGameForm.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { signInWithGoogle, signOut } from "@/lib/firebase/auth";
+
+function SignInButton() {
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={"/google.png"} alt="Sign in with Google" /> Sign in with Google
+    </button>
+  );
+}
+
+function SignOutButton() {
+  return <button onClick={() => signOut()}>Sign Out</button>;
+}
+
+export default async function UploadGameForm() {
+  const user = null;
+  const username = null;
+  // 1. user signed out <SignInButton />
+  // 2. user signed in, but missing username <UsernameForm />
+  // 3. user signed in, has username <SignOutButton />
+  return (
+    <>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
+    </>
+  );
+}
+
+function UsernameForm() {
+  return null;
+}
