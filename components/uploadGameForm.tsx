126969975715e0bcde0319cc62d265f7e060d31b
David Finol <davidmfinol@gmail.com>
2023-12-12 23:02:51 -0600

    Develop

--- components/uploadGameForm.tsx
+++ components/uploadGameForm.tsx
@@ -21,7 +21,7 @@ export default function UploadGameForm() {
     <>
       {user ? (
         username ? (
-          <SignOutButton />
+          <AutoUpdateUrlForm />
         ) : (
           <UsernameForm />
         )
@@ -146,6 +146,107 @@ function UsernameMessage({
   } else {
     return <p></p>;
   }
+}
+
+function AutoUpdateUrlForm() {
+  const [formValue, setFormValue] = useState("");
+  const [isValid, setIsValid] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { user, username } = useContext(UserContext);
+
+  const onSubmit = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+
+    // Create refs for both documents
+    const userDoc = doc(db, `users/${user?.uid}`);
+    const usernameDoc = doc(db, `usernames/${formValue}`);
+
+    // Commit both docs together as a batch write.
+    const batch = writeBatch(db);
+    batch.set(userDoc, {
+      username: formValue,
+    });
+    batch.set(usernameDoc, { uid: user?.uid });
+
+    await batch.commit();
+  };
+
+  const onChange = (e: { target: { value: string } }) => {
+    // Force form value typed in form to match correct format
+    const val = e.target.value.toLowerCase();
+    const re = /^(?=[a-zA-Z0-9._]{3,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+
+    // Only set form value if length is < 3 OR it passes regex
+    if (val.length < 3) {
+      setFormValue(val);
+      setLoading(false);
+      setIsValid(false);
+    }
+
+    if (re.test(val)) {
+      setFormValue(val);
+      setLoading(true);
+      setIsValid(false);
+    }
+  };
+
+  // Hit the database for username match after each debounced change
+  // useCallback is required for debounce to work
+  const checkUsername = useCallback(
+    debounce(async (username) => {
+      if (username.length >= 3) {
+        const ref = doc(db, `usernames/${username}`);
+        const exists = (await getDoc(ref)).exists();
+        console.log(
+          "Firestore read executed! " + username + " exists: " + exists,
+        );
+        setIsValid(!exists);
+        setLoading(false);
+      }
+    }, 500),
+    [],
+  );
+
+  useEffect(() => {
+    checkUsername(formValue);
+  }, [formValue, checkUsername]);
+
+  return (
+    <>
+      <div>
+        ( !username && (
+        <section>
+          <h2>Enter CGS AutoUpdate Url</h2>
+          <form onSubmit={onSubmit}>
+            <input
+              name="username"
+              placeholder="myname"
+              value={formValue}
+              onChange={onChange}
+            />
+            <UsernameMessage
+              username={formValue}
+              isValid={isValid}
+              loading={loading}
+            />
+            <button type="submit" className="btn-green" disabled={!isValid}>
+              Choose
+            </button>
+            <div>
+              Username: {formValue}
+              <br />
+              Loading: {loading.toString()}
+              <br />
+              Username Valid: {isValid.toString()}
+            </div>
+          </form>
+        </section>
+        )
+      </div>
+      <SignOutButton />
+    </>
+  );
 }
 
 function SignOutButton() {
