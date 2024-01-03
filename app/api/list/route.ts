1faa907459e71b2066bcdcb039e60a9f23da4e87
David Finol <davidmfinol@gmail.com>
2024-01-03 16:19:35 -0600

    Develop

--- app/api/list/route.ts
+++ app/api/list/route.ts
@@ -1,6 +1,6 @@
 import { getAllGames } from "@/lib/firebase/firestore";
 
 export async function GET() {
-    const allGames = await getAllGames();
-    return new Response(JSON.stringify(allGames));
+  const allGames = await getAllGames();
+  return new Response(JSON.stringify(allGames));
 }
