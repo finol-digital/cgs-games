d08edde3c83a913b5c3f1c1d5789d686da8d31f2
David Finol <davidmfinol@gmail.com>
2024-01-04 00:25:08 -0600

    Develop

--- app/[username]/[slug]/layout.tsx
+++ app/[username]/[slug]/layout.tsx
@@ -43,7 +43,7 @@ export default async function GameLayout({
         txt={game.name}
         title={"Play " + game.name}
       />
-      <section className="main-content">{children}</section>
+      <>{children}</>
       <FooterForGame game={game} />
     </main>
   );
