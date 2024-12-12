6be70b8f4c05e9238e2d1eb678b146ad5354647f
David Finol <davidmfinol@gmail.com>
2024-12-12 16:41:39 -0600

    Add responsiveness

--- components/header.tsx
+++ components/header.tsx
@@ -5,6 +5,7 @@ import MainNav from "./mainNav";
 import { UserContext } from "@/lib/context";
 import Link from "next/link";
 import { useContext } from "react";
+import MobileNav from "./mobileNav";
 import SignInButton from "./signInButton";
 import UserMenu from "./userMenu";
 
@@ -12,8 +13,9 @@ export default function Header() {
   const { user, username } = useContext(UserContext);
   return (
     <header className="bg-[#D3BD7A] sticky top-0 w-full border-b">
-      <div className="h-14 container flex items-center">
+      <div className="h-16 container flex items-center">
         <MainNav />
+        <MobileNav />
         <div className="flex items-center justify-end flex-1">
           {user ? (
             username ? (
---
+++ components/mobileNav.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
+import Link from "next/link";
+
+export default function MobileNav() {
+  return (
+    <div className="md:hidden">
+      <Sheet>
+        <SheetTrigger className="h-12">
+          <Menu className="h-6" />
+        </SheetTrigger>
+        <SheetContent side="left">
+          <nav>
+            <Link href="/" className="flex items-center ml-4">
+              <img
+                className="right-12"
+                // img instead of next/Image
+                src="cgs.png"
+                height="48"
+                alt="[CGS]"
+                //priority={true}
+              />
+              CGS Games
+            </Link>
+            <div className="flex flex-col gap-3 lg:gap-4 mt-6">
+              <Link href="/browse">Browse</Link>
+              <Link href="/upload">Upload</Link>
+            </div>
+          </nav>
+        </SheetContent>
+      </Sheet>
+    </div>
+  );
+}
