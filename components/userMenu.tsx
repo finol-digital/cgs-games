3fff127ccb67d6bc08744585dfccaca15b458bb5
David Finol <davidmfinol@gmail.com>
2024-12-12 14:56:06 -0600

    Update components/userMenu.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- components/userMenu.tsx
+++ components/userMenu.tsx
@@ -3,7 +3,7 @@
 import { signOut } from "@/lib/firebase/auth";
 import Link from "next/link";
 
-export default function UserMenu({ username }: { username: String }) {
+export default function UserMenu({ username }: { username: string }) {
   return (
     <div className="flex items-center">
       <Link href={`/${username}`}>{username}</Link>
