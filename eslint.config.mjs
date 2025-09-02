8f2b191be586b46597484331dd7d964929fe6a98
David Finol <davidmfinol@gmail.com>
2025-09-02 18:35:02 -0500

    Fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,27 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+
 import js from '@eslint/js';
 import { FlatCompat } from '@eslint/eslintrc';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
 });
 
-export default [{
-  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
-}, ...compat.extends('next/core-web-vitals', 'plugin:prettier/recommended'), {
-  rules: {
-    '@next/next/no-img-element': 'off',
+export default [
+  {
+    ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
   },
-}];
+  ...compat.extends(
+    'next/core-web-vitals',
+    'plugin:prettier/recommended'
+  ),
+  js.configs.recommended,
+  {
+    rules: {
+      '@next/next/no-img-element': 'off',
+    },
+  }
+];
