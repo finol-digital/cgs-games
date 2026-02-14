481deee68d7e7fcdab2027a88de9b70bd6470ba2
David Finol <davidmfinol@gmail.com>
2026-02-14 16:45:32 -0600

    Fix text contrast

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -18,30 +18,30 @@ const rawHTML = `
 [data-custom-class='title'], [data-custom-class='title'] * {
         font-family: Arial !important;
 font-size: 26px !important;
-color: #000000 !important;
+color: #f8fafc !important;
       }
 [data-custom-class='subtitle'], [data-custom-class='subtitle'] * {
         font-family: Arial !important;
-color: #595959 !important;
+color: #e2e8f0 !important;
 font-size: 14px !important;
       }
 [data-custom-class='heading_1'], [data-custom-class='heading_1'] * {
         font-family: Arial !important;
 font-size: 19px !important;
-color: #000000 !important;
+color: #f8fafc !important;
       }
 [data-custom-class='heading_2'], [data-custom-class='heading_2'] * {
         font-family: Arial !important;
 font-size: 17px !important;
-color: #000000 !important;
+color: #f8fafc !important;
       }
 [data-custom-class='body_text'], [data-custom-class='body_text'] * {
-        color: #595959 !important;
+        color: #e2e8f0 !important;
 font-size: 14px !important;
 font-family: Arial !important;
       }
 [data-custom-class='link'], [data-custom-class='link'] * {
-        color: #3030F1 !important;
+        color: #93c5fd !important;
 font-size: 14px !important;
 font-family: Arial !important;
 word-break: break-word !important;
