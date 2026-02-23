2d227961304c0ecb219b6714cb6a93c625ad0246
David Finol <davidmfinol@gmail.com>
2026-02-23 00:30:35 -0600

    Update API

--- app/api/gatcg_spoilers/route.ts
+++ app/api/gatcg_spoilers/route.ts
@@ -14,6 +14,8 @@ export async function GET() {
       card_image_url: string;
       back_card_name: string;
       back_card_image_url: string;
+      types: string[];
+      element: string;
     }[];
   } = {
     data: [],
@@ -25,6 +27,8 @@ export async function GET() {
       card_image_url: '',
       back_card_name: '',
       back_card_image_url: '',
+      types: [],
+      element: '',
     });
     dataContainer.data[i].uuid = '' + responseJson.spoilers[i].id;
     dataContainer.data[i].name = responseJson.spoilers[i].card_name;
@@ -33,6 +37,12 @@ export async function GET() {
       dataContainer.data[i].back_card_name = responseJson.spoilers[i].back_card.card_name;
       dataContainer.data[i].back_card_image_url =
         silvieHost + responseJson.spoilers[i].back_card.card_image_url;
+    }
+    if (responseJson.spoilers[i].card_type) {
+      dataContainer.data[i].types = [responseJson.spoilers[i].card_type];
+    }
+    if (responseJson.spoilers[i].element_name) {
+      dataContainer.data[i].element = responseJson.spoilers[i].element_name;
     }
   }
   console.log(dataContainer);
